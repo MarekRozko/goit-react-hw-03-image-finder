ab2b9b625241b676889edd2c1784e64a3c26f2e3
Marek Rożko <maknamar12@gmail.com>
2023-01-30 18:39:03 +0100

    add fixed

--- src/components/Modal/ModalDetails.jsx
+++ src/components/Modal/ModalDetails.jsx
@@ -1,8 +1,8 @@
 import PropTypes from 'prop-types';
 
-const ModalDetails = ({ largeImageURL, tags }) => {
+const ModalDetails = ({ largeImageURL }) => {
     return (
-        <img src={largeImageURL} alt={tags} />
+        <img src={largeImageURL} alt="" />
     )
 }
 
@@ -11,5 +11,4 @@ export default ModalDetails;
 
 ModalDetails.propTypes = {
   largeImageURL: PropTypes.string.isRequired,
-  tags: PropTypes.string.isRequired,
 };
